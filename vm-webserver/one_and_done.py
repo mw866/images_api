e224948057bf68f52d5ba27b40ac6d28d9ef6c68
Chris Wang <mw866@cornell.edu>
2016-12-01 03:16:25 +0000

    fixed incorrect scores and improved caching

--- vm-webserver/one_and_done.py
+++ vm-webserver/one_and_done.py
@@ -1,19 +1,17 @@
 from flask import Flask, request, abort
 import os
 import requests
-from StringIO import StringIO
-from wand.image import Image
 from urlparse import urlparse 
 from tempfile import NamedTemporaryFile
 import commands
-# import datetime
 import logging
 
 
-url_tmpfile_dict = {}
+
+url_colorcount_dict = {}
 app = Flask(__name__)	
-# handler = logging.FileHandler('flask.log')
-# handler.setLevel(logging.INFO)
+# handler = logging.FileHandler('/var/log/flask/error.log')
+# handler.setLevel(logging.ERROR)
 # app.logger.addHandler(handler)
 
 @app.route("/")
@@ -22,30 +20,30 @@ def home():
 
 @app.route("/api/num_colors")
 def num_colors():
-	global url_count_dict 
+	global url_colorcount_dict 
 	tmpfilepath = ""
 	url = request.args.get('src')
 	# if not cached
-	if url not in url_tmpfile_dict:
+	if url not in url_colorcount_dict:
 		app.logger.info("cache: miss")
 		r = requests.get(url, timeout=5)
 		f, file_ext = os.path.splitext(os.path.basename(urlparse(url).path))
 		if 'image' not in r.headers['content-type']:
 			app.logger.error(url + " is not an image.")
 			abort(400, url + " is not an image.")
-		with Image(file=StringIO(r.content)) as img:
-			with  NamedTemporaryFile(mode='w+b',suffix=img.format, delete=False) as temp_file:
-				img.save(file=temp_file)
-				temp_file.seek(0,0)
-				url_tmpfile_dict[url] = temp_file.name
-				tmpfilepath = temp_file.name
+		with  NamedTemporaryFile(mode='w+b',suffix=file_ext, delete=False) as temp_file:
+			temp_file.write(r.content)
+			temp_file.seek(0,0)
+			command = "/usr/bin/identify -format %k " + temp_file.name
+			app.logger.debug(command)
+			colorcount = commands.getoutput(command)
+			url_colorcount_dict[url] = colorcount
+
 	else: 	# if cached
-		app.logger.info("cache: hit at")
-		tmpfilepath = url_tmpfile_dict[url]
-	app.logger.info(tmpfilepath)
-	command = "/usr/bin/identify -format %k " + tmpfilepath
-	color_count = commands.getoutput(command)
-	return color_count
+		app.logger.debug("cache: hit")
+		colorcount = url_colorcount_dict[url]
+	app.logger.debug(colorcount)
+	return colorcount
 
 if __name__ == "__main__":
 	app.run() #if run locally ane exposed to public network
