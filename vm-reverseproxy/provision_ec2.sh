660f38536c1e1798e29d73a48ea33a46143064c6
Chris M Wang <mw866@cornell.edu>
2016-12-01 22:01:59 -0500

    minor changes

--- vm-reverseproxy/provision_ec2.sh
+++ vm-reverseproxy/provision_ec2.sh
@@ -10,14 +10,14 @@ set -e
 set -x
 
 echo "Provisioning Reverse Proxy on EC2!"
-
+sudo echo "reverseproxy" > /etc/hostname
 sudo apt-get update
 sudo apt-get install -y nginx lynx 
 
 #Config NGINX:
 sudo cd ~/one-and-done/vm-reverseproxy
-sudo ln -s "~/one_and_done" /etc/nginx/sites-available/
-sudo ln -s "~/one_and_done" /etc/nginx/sites-enabled/
+sudo ln -s ~/one-and-done/vm-reverseproxy/one_and_done /etc/nginx/sites-available/
+sudo ln -s ~/one-and-done/vm-reverseproxy/one_and_done /etc/nginx/sites-enabled/
 sudo systemctl restart nginx
 
 
